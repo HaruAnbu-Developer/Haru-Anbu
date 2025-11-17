b8d5ab48f3532996ef76f82a09dd3c3a052530c1
snsn8105 <snsn8105@naver.com>
2025-11-17 20:07:32 +0900

    chore: call manager 모듈 초기 설정

---
+++ CallManager/src/main/java/com/haru_anbu/CallManager/CallManagerApplication.java
@@ -0,0 +1,13 @@
+package com.haru_anbu.CallManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallManagerApplication.class, args);
+	}
+
+}
---
+++ CallManager/src/test/java/com/haru_anbu/CallManager/CallManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru_anbu.CallManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
